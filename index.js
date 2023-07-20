f6814f826d1356b615296ce2557cade75b807648
Ajenifuja Sodiq <sodeeqajenifuja1.email>
2023-07-19 23:14:48 -1100

    save2

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 let PORT = process.env.PORT
 let URI = process.env.MONGO_URI
